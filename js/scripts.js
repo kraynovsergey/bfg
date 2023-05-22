2cfe6dbe8e31a409fd50d137e0d85801bad32ad9
sergeykraynov <kraynov.sergey@gmail.com>
2023-05-22 14:35:19 +0300

    Add services page

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,7 @@
 		portfolioClickFollower = document.querySelector(".portfolio__item-click-follower");
 	if ( portfolioClick && portfolioClickFollower ) {
 		let imgs = document.querySelectorAll(".portfolio__item-img"),
+			servicesItems = document.querySelectorAll('.services__item'),
 			posX = 0,
 			posY = 0,
 			mouseX = 0,
@@ -39,14 +40,41 @@
 			mouseY = e.pageY;
 		});
 
-		imgs.forEach(function(elem, index){
-			elem.addEventListener('mouseenter', e => {
-				portfolioClickFollower.classList.add('_active');
+		if (imgs.length > 0) {
+			imgs.forEach(function (elem, index) {
+				elem.addEventListener('mouseenter', e => {
+					portfolioClickFollower.classList.add('_active');
+				});
+				elem.addEventListener('mouseleave', e => {
+					portfolioClickFollower.classList.remove('_active');
+				});
 			});
-			elem.addEventListener('mouseleave', e => {
-				portfolioClickFollower.classList.remove('_active');
+		}
+
+		if (servicesItems.length > 0) {
+			servicesItems.forEach(function (elem, index) {
+				elem.addEventListener('mouseenter', e => {
+					let dataItem = elem.getAttribute('data-item'),
+						dataSwipers = document.querySelectorAll('.services__follower-swiper');
+
+					portfolioClickFollower.classList.add('_active');
+
+					if ( dataSwipers.length > 0 ) {
+						dataSwipers.forEach(item => {
+							let dItem = item.getAttribute('data-item');
+							if ( dItem === dataItem ) {
+								item.classList.add('_active');
+							} else {
+								item.classList.remove('_active');
+							}
+						});
+					}
+				});
+				elem.addEventListener('mouseleave', e => {
+					portfolioClickFollower.classList.remove('_active');
+				});
 			});
-		});
+		}
 	}
 
 	let typeSplit = new SplitType('.btn__title', {
@@ -86,4 +114,57 @@
 	let mm = gsap.matchMedia();
 
 	mm.add("(min-width: 992px)", (() => (mainButtonHover(), () => {})));
+
+	const servicesItems = document.querySelectorAll('.services__item');
+	if ( servicesItems.length > 0 ) {
+		servicesItems.forEach(item => {
+			$(item).on( "click", function() {
+				item.classList.toggle('_active');
+				$(this).find('.services__item-content').slideToggle('slow');
+			});
+		});
+	}
+
+	const servicesFollowersSwiper = document.querySelectorAll('.services__follower-swiper');
+	if ( servicesFollowersSwiper.length > 0 ) {
+		servicesFollowersSwiper.forEach((item) => {
+			new Swiper(item, {
+				effect: 'fade',
+				speed: 500,
+				fadeEffect: {
+					crossFade: true
+				},
+				autoplay: {
+					delay: 500,
+					disableOnInteraction: false
+				}
+			});
+		});
+	}
+
+	$(".services__item-link").mouseenter(function (e) {
+		var parentOffset = $(this).offset();
+
+		var relX = e.pageX - parentOffset.left;
+		var relY = e.pageY - parentOffset.top;
+		$(this).find(".services__item-link-circle").css({
+			left: relX,
+			top: relY,
+		});
+		$(this).find(".services__item-link-circle").removeClass("desplode-circle");
+		$(this).find(".services__item-link-circle").addClass("explode-circle");
+	});
+
+	$(".services__item-link").mouseleave(function (e) {
+		var parentOffset = $(this).offset();
+
+		var relX = e.pageX - parentOffset.left;
+		var relY = e.pageY - parentOffset.top;
+		$(this).find(".services__item-link-circle").css({
+			left: relX,
+			top: relY,
+		});
+		$(this).find(".services__item-link-circle").removeClass("explode-circle");
+		$(this).find(".services__item-link-circle").addClass("desplode-circle");
+	});
 })();
